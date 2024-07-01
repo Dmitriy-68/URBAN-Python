c1b8dc9e9bf62a13607265fc248ffad8d8d18af8
Dmitriy-68 <d73-68@yandex.ru>
2024-07-01 17:19:25 +0300

    Module 3 Homework 9 Распаковка позиционных параметров

---
+++ module3/module_3_3.py
@@ -0,0 +1,20 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+# 1.Функция с параметрами по умолчанию:
+print_params()
+print_params(7)
+print_params(b = 502)
+print_params('xxx', 'ooo')
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+# 2.Распаковка параметров:
+values_list = ['string', 74, (1, 2)]
+values_dict = {'a': [4, 5, 8], 'b': False, 'c': 998877}
+print_params(*values_list)
+print_params(**values_dict)
+
+# 3.Распаковка + отдельные параметры:
+values_list_2 = [777, 'IIIOOOIII']
+print_params(*values_list_2, 42)
