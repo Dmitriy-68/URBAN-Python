280b71c669b02c4ba36302befd0a3af3446b0916
Dmitriy-68 <d73-68@yandex.ru>
2024-07-01 19:44:33 +0300

    Module 3 Homework 4 Произвольное число параметров

--- module3/module_3_4.py
+++ module3/module_3_4.py
@@ -1,11 +1,11 @@
 def single_root_words(root_word, *other_words):
     same_words = []
-    for i in other_words:
-        if root_word.upper() in i.upper() or i.upper() in root_word.upper():
-            same_words.append(i)
+    for word in other_words:
+        if root_word.upper() in word.upper() or word.upper() in root_word.upper():
+            same_words.append(word)
     return same_words
 
-result1 = single_root_words('гор', 'горный', 'разгорелся', 'начало', 'конец', 'горница')
-result2 = single_root_words('доступ', 'ступ', 'заступ', 'недоступный', 'рука')
+result1 = single_root_words('гОр', 'горный', 'разгорелся', 'начало', 'конец', 'горница')
+result2 = single_root_words('доСтУп', 'ступ', 'заступ', 'недоступный', 'рука')
 print(result1)
 print(result2)
