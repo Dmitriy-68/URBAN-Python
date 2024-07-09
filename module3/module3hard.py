e5aaa1910c877241c23805caccb387f0ed5851c0
Dmitriy-68 <d73-68@yandex.ru>
2024-07-09 19:53:56 +0300

    Module 3 Homework 5 Рекурсия

---
+++ module3/module3hard.py
@@ -0,0 +1,27 @@
+def calculate_structure_sum(a):
+    global sum
+    for i in range(len(a)):
+        if isinstance(a[i], dict):
+            calculate_structure_sum(list(a[i].keys()))
+            calculate_structure_sum(list(a[i].values()))
+        elif isinstance(a[i], set):
+            calculate_structure_sum(list(a[i]))
+        elif isinstance(a[i], int | float):
+            sum += a[i]
+        elif isinstance(a[i], str):
+            sum += len(a[i])
+        else:
+            calculate_structure_sum(a[i])
+    return sum
+
+
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+sum = 0
+result = calculate_structure_sum(data_structure)
+print(result)
